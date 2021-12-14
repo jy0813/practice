169bd6177074941ca8a203b77730459e26804e93
jy0813 <75197188+jy0813@users.noreply.github.com>
2021-12-14 21:20:26 +0900

    shopping_list 만들기

---
+++ src/assets/js/main.js
@@ -0,0 +1,62 @@
+'use stric;'
+
+const items = document.querySelector('.items')
+const input = document.querySelector('.footer_input')
+const addBtn = document.querySelector('.footer_btn')
+
+function onAdd() {
+  // 1. 사용자가 입력한 텍스트를 받아온다.
+  const text = input.value;
+  if(text === '') {
+    input.focus();
+    return;
+  }
+  // 2. 새로운 아이템을 만든다. (텍스트 + 삭제 버튼)
+  const item = createItem(text);
+  // 3. items 컨테이너 안에 새로 만든 아이템을 추가
+  items.appendChild(item);
+  // 4. 새로 추가된 아이템으로 스크롤링 
+  item.scrollIntoView({block:'center', behavior: 'smooth'});
+  // 5. addBtn을 누르면 inout을 초기화
+  input.value = '';
+  input.focus();
+}
+
+function createItem(text) {
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item_row');
+
+  const item = document.createElement('div');
+  item.setAttribute('class', 'item');
+
+  const name = document.createElement('span');
+  name.setAttribute('class', 'item_name');
+  name.innerText = text;
+
+  const btn = document.createElement('button');
+  btn.setAttribute('class', 'item_delete');
+  btn.innerHTML = '<i class="fas fa-trash-alt"></i>'
+  btn.addEventListener('click', ()=> {
+    items.removeChild(itemRow);
+  })
+
+  const itemDivider = document.createElement('div');
+  itemDivider.setAttribute('class', 'item_divider');
+
+  item.appendChild(name);
+  item.appendChild(btn);
+
+  itemRow.appendChild(item);
+  itemRow.appendChild(itemDivider);
+  return itemRow;
+}
+
+addBtn.addEventListener('click', () => {
+  onAdd();
+})
+
+input.addEventListener('keypress', event => {
+  if(event.key === 'Enter') {
+    onAdd();
+  } 
+})
