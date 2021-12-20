c5c07a73c907b513a314f460f919b6dfe9a81e4c
진재윤 <jy0813>
2021-12-20 16:58:46 +0900

    bubble 이벤트

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,7 +7,7 @@ const addBtn = document.querySelector('.footer_btn')
 function onAdd() {
   // 1. 사용자가 입력한 텍스트를 받아온다.
   const text = input.value;
-  if(text === '') {
+  if (text === '') {
     input.focus();
     return;
   }
@@ -16,38 +16,54 @@ function onAdd() {
   // 3. items 컨테이너 안에 새로 만든 아이템을 추가
   items.appendChild(item);
   // 4. 새로 추가된 아이템으로 스크롤링 
-  item.scrollIntoView({block:'center', behavior: 'smooth'});
+  item.scrollIntoView({
+    block: 'center',
+    behavior: 'smooth'
+  });
   // 5. addBtn을 누르면 inout을 초기화
   input.value = '';
   input.focus();
 }
 
+let id = 0; //UUID
+
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item_row');
+  itemRow.setAttribute('data-id', id);
 
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
+  itemRow.innerHTML = `
+  <div class="item">
+    <span class="item_name">${text}</span>
+    <button class="item_delete">
+      <i class="fas fa-trash-alt" data-id=${id}></i>
+    </button>
+  </div>
+  <div class="item_divider"></div>`;
+  id++;
 
-  const name = document.createElement('span');
-  name.setAttribute('class', 'item_name');
-  name.innerText = text;
+  // const item = document.createElement('div');
+  // item.setAttribute('class', 'item');
 
-  const btn = document.createElement('button');
-  btn.setAttribute('class', 'item_delete');
-  btn.innerHTML = '<i class="fas fa-trash-alt"></i>'
-  btn.addEventListener('click', ()=> {
-    items.removeChild(itemRow);
-  })
+  // const name = document.createElement('span');
+  // name.setAttribute('class', 'item_name');
+  // name.innerText = text;
 
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'item_divider');
+  // const btn = document.createElement('button');
+  // btn.setAttribute('class', 'item_delete');
+  // btn.innerHTML = '<i class="fas fa-trash-alt"></i>'
+  // btn.addEventListener('click', ()=> {
+  //   items.removeChild(itemRow);
+  // })
 
-  item.appendChild(name);
-  item.appendChild(btn);
+  // const itemDivider = document.createElement('div');
+  // itemDivider.setAttribute('class', 'item_divider');
 
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  // item.appendChild(name);
+  // item.appendChild(btn);
+
+  // itemRow.appendChild(item);
+  // itemRow.appendChild(itemDivider);
   return itemRow;
 }
 
@@ -55,8 +71,20 @@ addBtn.addEventListener('click', () => {
   onAdd();
 })
 
-input.addEventListener('keypress', event => {
-  if(event.key === 'Enter') {
+input.addEventListener('keydown', event => {
+  if(event.isComposing) {
+    return;
+  }
+  if (event.key === 'Enter') {
     onAdd();
-  } 
+  }
+});
+
+items.addEventListener('click', event => {
+  const id =event.target.dataset.id;
+  if(id) {
+    const toBeDeleted = document.querySelector(`.item_row[data-id="${id}"]`)
+    toBeDeleted.remove();
+  }
 })
+
