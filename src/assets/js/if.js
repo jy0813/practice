c9c5ec34adc1ebc88dfed6da3da4e898aebdccf1
jy0813 <75197188+jy0813@users.noreply.github.com>
2021-12-21 19:44:41 +0900

    조건문 활용

---
+++ src/assets/js/if.js
@@ -0,0 +1,57 @@
+'use stric;'
+
+const btn = document.querySelector('.btn');
+const modalWrap = document.querySelector('.modal_wrap');
+const close = document.querySelector('.close');
+const input = document.querySelector('.test_input');
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+
+document.querySelector('form').addEventListener('submit', function(e) {
+  if(email.value == '') {
+    e.preventDefault();
+    document.querySelector('.error_txt').style.display = "block";
+  }
+});
+
+
+btn.addEventListener('click', () => {
+  modalWrap.style.display = "block";
+});
+
+close.addEventListener('click', () => {
+  modalWrap.style.display = "none";
+})
+
+if(1 == 3) {
+  console.log('안녕');
+} else if (1 < 2) {
+  console.log('안녕2');
+} else {
+  console.log('반가워');
+}
+
+if(1 == 2 && 2 == 2) { //전부 참일때
+  console.log('모두 맞앗엉');
+} else {
+  console.log('모두 만족해야해');
+}
+
+if(1 == 2 || 2 == 2) {
+  console.log('한개는 맞아!');
+}
+
+const n = '';
+
+function 삼육구게임() {
+  if(n % 9 == 0) {
+    console.log('짝짝!');
+  } else if(n % 3 == 0) {
+    console.log('짝!');
+  }
+  else {
+    console.log('통과!')
+  }
+}
+
+삼육구게임();
