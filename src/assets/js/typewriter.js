1a5579a49e9efc3d4bfaac6d17e8e8a7cb34636b
jy0813 <75197188+jy0813@users.noreply.github.com>
2022-01-04 22:15:52 +0900

    type

---
+++ src/assets/js/typewriter.js
@@ -0,0 +1,11 @@
+'use stric;'
+
+const btn = document.querySelector('.btn');
+const h1 = document.querySelector('h1');
+const h1Default = document.querySelector('h1').innerHTML;
+
+btn.addEventListener('click', () => {
+  h1.innerHTML = '';
+
+  for (let i = 0; i < )
+})
