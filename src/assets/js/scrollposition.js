e81b40a4ea14f9515c6ce9845a13765239739a49
jy0813 <75197188+jy0813@users.noreply.github.com>
2022-01-04 20:12:57 +0900

    scroll opacity 조절

---
+++ src/assets/js/scrollposition.js
@@ -0,0 +1,12 @@
+'use stric;'
+
+window.addEventListener('scroll', () => {
+  const scrollTop = window.scrollY;
+  const card = document.querySelectorAll('.card-box');
+  console.log(scrollTop);
+    const y = (-1/500) * scrollTop + 115/50
+    const z = (-1/5000) * scrollTop + 500/500;
+    card[0].style.opacity = y;
+    card[0].style.transform = `scale(${z})`;
+})
+
