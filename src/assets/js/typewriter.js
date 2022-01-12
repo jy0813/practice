743176b3a1a874fc473f7bd353d84d0820ea6b7d
jy0813 <busbug3@gmail.com>
2022-01-12 09:41:14 +0900

    타이핑

--- src/assets/js/typewriter.js
+++ src/assets/js/typewriter.js
@@ -4,8 +4,12 @@ const btn = document.querySelector('.btn');
 const h1 = document.querySelector('h1');
 const h1Default = document.querySelector('h1').innerHTML;
 
-btn.addEventListener('click', () => {
+window.addEventListener('load', () => {
   h1.innerHTML = '';
 
-  for (let i = 0; i < )
+  for (let i = 0; i < h1Default.length; i++) {
+    setTimeout(function() {
+      h1.innerHTML = h1.innerHTML + h1Default[i]
+    }, i * 500);
+  }
 })
